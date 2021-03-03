c58f6fcf11484255c3952a7143027ce183b97c77
alawani <alawani@cisco.com>
2021-03-03 10:42:11 -0600

    creating multiple validators

--- src/app/welcome/profile.component.ts
+++ src/app/welcome/profile.component.ts
@@ -1,6 +1,6 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './profile.component.html',
@@ -8,19 +8,31 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup
+  private firstName;
+  private lastName;
 
   constructor() { }
 
   ngOnInit(): void {
-    let firstName = new FormControl();
-    let lastName = new FormControl();
+    this.firstName = new FormControl("", [Validators.required, Validators.pattern('[a-zA-Z].*')]);
+    this.lastName = new FormControl("", [Validators.required, Validators.pattern('[a-zA-Z].*')]);
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
-  saveProfile(profileForm){
-    alert(`Your profile has been saved as First Name: ${profileForm.firstName} and Last Name: ${profileForm.lastName}`)
+  saveProfile(formValues){
+    if(this.profileForm.valid){
+      alert(`Your profile has been saved as First Name: ${formValues.firstName} and Last Name: ${formValues.lastName}`)
+    }else{
+      alert("All fields are required please!")
+    }
+  }
+  validateFirstName(){
+    return this.firstName.valid || this.firstName.untouched
+  }
+  validateLastName(){
+    return this.lastName.valid || this.lastName.untouched
   }
 
 }
